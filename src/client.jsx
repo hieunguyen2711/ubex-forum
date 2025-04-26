decb5477d70c6edfb53ddf79dfa5137cc61c5c56
Hieu Nguyen <davidthehieu@gmail.com>
2025-04-25 22:04:06 -0500

    Added firebase and supabase api to gitignore

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const URL = 'https://byexehunbujcboxbrscp.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5ZXhlaHVuYnVqY2JveGJyc2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzNzU5NDEsImV4cCI6MjA2MDk1MTk0MX0.oXvAIxTYL4rcIfY79PNuCSZYiEMlOytFgyMkMg90yq0';
-
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 export const supabase = createClient(URL, API_KEY);
 
